5cd368c62fd87fd4fd686766a859dcb3eb847601
noam.edery@tibaparking.com <noam.edery@tibaparking.com>
2025-12-22 16:26:05 +0200

    fix fix

--- scraper_viagogo.py
+++ scraper_viagogo.py
@@ -181,7 +181,6 @@ def get_driver():
             options.add_argument('--disable-features=VizDisplayCompositor')
             options.add_argument('--disable-extensions')
             options.add_argument('--window-size=1920,1080')
-            options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36')
             options.page_load_strategy = 'eager' 
 
         browser_path = '/usr/bin/chromium' if os.path.exists('/usr/bin/chromium') else None
